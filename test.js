0fe3541241110de331d8b5ca7764df862e6de722
dpetrov <daniel.petrov@tick42.com>
2017-05-11 12:30:10 +0300

    gitignore

---
+++ test.js
@@ -0,0 +1,12 @@
+var webdriverio = require('webdriverio');
+var options = {
+    desiredCapabilities: {
+        browserName: 'firefox'
+    }
+};
+webdriverio
+    .remote(options)
+    .init()
+    .url('http://buddy.works')
+    .saveScreenshot('buddyworks.png')
+    .end();
